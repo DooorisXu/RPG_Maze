8f42a4284cff848b6b83614b70e7fc00f9ff04f9
DooorisXu <lingyi19329@gmail.com>
2017-12-25 19:14:48 -0500

    Fixed the while loop--it's a dumb indentation problem Need to work on game details :)

--- RPG.py
+++ RPG.py
@@ -1,5 +1,10 @@
 def instructions():
     print("RPG Game \n ======= \n Commands: \n go[direction] \n get[item]")
+    print('You have to escape the house \n using the commands above ')
+    print('To escape, you need to go to the garden \n with a key and a magic potion')
+    print('If you walk into a room with a monster in it \n You lose the game ')
+    print('=======')
+
 
 
 def status():
@@ -46,52 +51,46 @@ rooms = {
 currentRoom = 'Hall'
 
 instructions()
-
-#loop infinitely
 while True:
     status()
+
 #get the player's move
 #.split breaks it up into an list array
 #eg: typing 'go east' would give the list ['go','east']
-
-move = ''
-while move == '':
-    move = input ('>')
+    move = ''
+    while move == '':
+        move = input ('>')
     # player loses if the enter a room with a monster
-    if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
-        print('A monster has got you...\n GAME OVER')
+        if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
+            print('A monster has got you...\n GAME OVER')
         break
     # player wins if they get to the garden with a key and the magic potion
-    if currentRoom == 'Garden' and 'key' in inventory and 'magic potion' in inventory:
-        print('You escaped the house...\n YOU WON!')
+        if currentRoom == 'Garden' and 'key' in inventory and 'magic potion' in inventory:
+         print('You escaped the house...\n YOU WON!')
         break
 
-move = move.lower().split()
+    move = move.lower().split()
 
-#if they type 'go' first
-if move[0] == 'go':
-    #check if the move is allowed
-    if move [1] in rooms [currentRoom]:
-        #set the current room to the new room
-        currentRoom = rooms [currentRoom][move[1]]
-        #there is no door (link) to the new room
-    else:
-        print('You can\'t go that way!')
+    #if they type 'go' first
+    if move[0] == 'go':
+        #check if the move is allowed
+        if move [1] in rooms [currentRoom]:
+            #set the current room to the new room
+            currentRoom = rooms [currentRoom][move[1]]
+            #there is no door (link) to the new room
+        else:
+            print('You can\'t go that way!')
 
-#if they type 'get' first
-if move [0] == 'get':
-    #if the move is allowed
-    if 'item' in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
-        #add the item to the inventory
-        inventory += move[1]
-        #display a movement complete message
-        print (move[1] + ' got!')
-        #delete the item from the room
-        del rooms[currentRoom]['item']
-    #otherwise (the item is not there to get)
-    else:
-        print('Can\'t get ', move[1], ' !')
-
-
-
-
+    #if they type 'get' first
+    if move [0] == 'get':
+        #if the move is allowed
+        if 'item' in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
+            #add the item to the inventory
+            inventory += move[1]
+            #display a movement complete message
+            print (move[1] + ' got!')
+            #delete the item from the room
+            del rooms[currentRoom]['item']
+        #otherwise (the item is not there to get)
+        else:
+            print('Can\'t get ', move[1], ' !')
